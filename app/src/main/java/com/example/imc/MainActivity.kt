e433b73400565e688355da301c0c3cc481871050
gesley <gesley.apoioprodesp.sp.gov.br>
2021-06-17 20:29:11 -0300

    aula 17/06

--- app/src/main/java/com/example/imc/MainActivity.kt
+++ app/src/main/java/com/example/imc/MainActivity.kt
@@ -6,6 +6,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.example.imc.watchers.DecimalTextWatcher
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,6 +34,24 @@ class MainActivity : AppCompatActivity() {
         val peso = etPeso.text.toString().toDouble()
         val altura = etAltura.text.toString().toDouble()
         val imc = peso / (peso * altura)
+
+        when(imc) {
+            in 0.0..18.5 -> configuraIMC(imc, R.drawable.masc_abaixo, R.string.magreza)
+            in 18.6..24.9 -> configuraIMC(imc, R.drawable.masc_ideal, R.string.peso_normal)
+            in 25.0..29.9 -> configuraIMC(imc, R.drawable.masc_sobre, R.string.sobre_peso)
+            in 30.0..34.9 -> configuraIMC(imc, R.drawable.masc_obeso, R.string.obesidade_grau_i)
+            in 35.0..39.9 -> configuraIMC(imc, R.drawable.masc_extremo_obeso, R.string.obesidade_grau_ii)
+            else -> configuraIMC(imc, R.drawable.masc_extremo_obeso, R.string.obesidade_grau_iii)
+        }
+    }
+
+
+    private fun configuraIMC(imc: Double, drawableId: Int, stringId: Int) {
+        tvIMC.text = getString(R.string.status_imc, imc)
+        ivIMCStatus.setImageDrawable(
+            ContextCompat.getDrawable(this, drawableId)
+        )
+        tvIMCStatus.text = getString(stringId)
     }
 
     private fun setUpView() {
@@ -41,6 +61,9 @@ class MainActivity : AppCompatActivity() {
         tvIMC = findViewById(R.id.tvIMC)
         tvIMCStatus = findViewById(R.id.tvIMCStatus)
         ivIMCStatus = findViewById(R.id.ivIMCStatus)
+
+        etPeso.addTextChangedListener(DecimalTextWatcher(etPeso, 1))
+        etAltura.addTextChangedListener(DecimalTextWatcher(etAltura, 2))
 
     }
 }
